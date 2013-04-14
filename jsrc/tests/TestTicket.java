84f7941fb0611b8986a891b097514719bf230e3e
Wes Unwin <wes_unwin@hotmail.com>
2013-04-14 19:52:58 -0400

    starting testTicket

--- jsrc/tests/TestTicket.java
+++ jsrc/tests/TestTicket.java
@@ -1,6 +1,7 @@
 
 package tests;
 
+import assets.*;
 import transaction.*;
 import java.util.zip.DataFormatException;
 import java.util.Vector;
@@ -14,8 +15,23 @@ import org.junit.runners.JUnit4;
 @RunWith(JUnit4.class)
 public class TestTicket {
 
+	//BEING WORKED ON BY WES, STILL HAVE MANY TESTS TO ADD HERE
 
+	//Test 1: Valid construction of a ticket object, given a correctly formatted ATF File Line
+	@Test
+	public void testTicket_Valid() throws DataFormatException
+	{
+		Ticket t = new Ticket("EventName1222223333 Username1122222 100 102.49");
+	}
 
+	//Test 2: Tests for failure of ticket creation due to incorrect ATF File Line size
+	@Test(expected = DataFormatException.class)
+	public void testTicket_InvalidLineLength() throws DataFormatException
+	{
+		Ticket t = new Ticket("IncorrectLengthOfThisLIne");
+	}
+
+	
 
 
 
