1e1bad5a8b2581177659ac5d295a6792c20ecd7f
BrittusNightmare <superflash54@gmail.com>
2013-04-15 14:57:53 -0400

    Fixed a few Errors

--- jsrc/TestAddCredit.java
+++ jsrc/TestAddCredit.java
@@ -20,37 +20,37 @@ public class TestAddCredit{
 	}
 	//Test constructors ability to indentify an invalid usename
 	@Test
-	public void testAddCreditConstruct_InvalidUseName{
+	public void testAddCreditConstruct_InvalidUseName() throws DataFormatException{
 		AddCredit TestAddCredit = new AddCredit("UserNa&e1122222 FS 000100.00");
 	}
 	//Tests constructors ability to catch an invalid usertype
 	@Test
-	public void testAddCreditConstruct_InvalidUseType{
+	public void testAddCreditConstruct_InvalidUserType() throws DataFormatException{
 		AddCredit TestAddCredit = new AddCredit("UserName1122222 RS 000100.00");
 	}
 	//Tests constructors ability to catch an invalid credit value
 	@Test
-	public void testAddCreditConstruct_InvalidCreditValue{
+	public void testAddCreditConstruct_InvalidCreditValue() throws DataFormatException{
 		AddCredit TestAddCredit = new AddCredit("UserName1122222 FS 0001k0.00");
 	}
 	//Tests constructors ability to prevent overcrediting per session
 	@Test
-	public void testAddCreditConstruct_InvalidCreditAmounnt{
+	public void testAddCreditConstruct_InvalidCreditAmounnt() throws DataFormatException{
 		AddCredit TestAddCredit = new AddCredit("UserName1122222 FS 010000.00");
 	}
 	//Tests constructors ability to reject incorrect string formats
 	@Test
-	public void testAddCreditConstruct_InvalidCharStream{
+	public void testAddCreditConstruct_InvalidStringLength() throws DataFormatException{
 		AddCredit TestAddCredit = new AddCredit("UserName11222227 RSW 000100.000");
 	}
 	//Tests constructors ability to reject zero addition
 	@Test
-	public void testAddCreditConstruct_ZeroValue{
+	public void testAddCreditConstruct_ZeroValue() throws DataFormatException{
 		AddCredit TestAddCredit = new AddCredit("UserName11222227 FS 000000.00");
 	}
 	//Tests constructors ability to catch decimal values
 	@Test
-	public void testAddCreditConstruct_DecimalInserted{
+	public void testAddCreditConstruct_DecimalInserted() throws DataFormatException{
 		AddCredit TestAddCredit = new AddCredit("UserName1122222 FS 000100.99");
 	}
 }
--- jsrc/TestRefund.java
+++ jsrc/TestRefund.java
@@ -19,37 +19,37 @@ public class TestRefund{
 	}
 	//Test constructors ability to indentify an invalid buyer usename
 	@Test
-	public void testRefundConstruct_InvalidBuyerUseName{
+	public void testRefundConstruct_InvalidBuyerUseName() throws DataFormatException{
 		Refund TestRefund = new Refund("UserNa&e1122222 UserName3344444 000100.00");
 	}
 	//Test constructors ability to indentify an invalid usename
 	@Test
-	public void testRefundConstruct_InvalidSellerUseName{
+	public void testRefundConstruct_InvalidSellerUseName() throws DataFormatException{
 		Refund TestRefund = new Refund("UserName1122222 UserN#me3344444 000100.00");
 	}
 	//Tests constructors ability to catch an invalid credit value
 	@Test
-	public void testRefundConstruct_InvalidCreditValue{
+	public void testRefundConstruct_InvalidCreditValue() throws DataFormatException{
 		Refund TestRefund = new Refund("UserName1122222 UserName3344444 0001k0.00");
 	}
 	//Tests constructors ability to prevent overcrediting per session
 	@Test
-	public void testRefundConstruct_InvalidCreditAmounnt{
+	public void testRefundConstruct_InvalidCreditAmounnt() throws DataFormatException{
 		Refund TestRefund = new Refund("UserName1122222 UserName3344444 010000.00");
 	}
 	//Tests constructors ability to reject incorrect string formats
 	@Test
-	public void testRefundConstruct_InvalidCharStream{
+	public void testRefundConstruct_InvalidStringLength() throws DataFormatException{
 		Refund TestRefund = new Refund("UserName11222227 UserName33444445 000100.000");
 	}
 	//Tests constructors ability to reject zero addition
 	@Test
-	public void testRefundConstruct_ZeroValue{
+	public void testRefundConstruct_ZeroValue() throws DataFormatException{
 		Refund TestRefund = new Refund("UserName11222227 UserName3344444 000000.00");
 	}
 	//Tests constructors ability to catch decimal values
 	@Test
-	public void testRefundConstruct_DecimalInserted{
+	public void testRefundConstruct_DecimalInserted() throws DataFormatException{
 		Refund TestRefund = new Refund("UserName1122222 UserName3344444 000100.99");
 	}
 }
